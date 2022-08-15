0d9bebe87469ad50bec7726d31d1b7ca120d0ac2
lucasmch <lucasgms2002@gmail.com>
2022-08-14 21:07:30 -0300

    add preloader

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,5 @@
 $(window).on('load', function () {
   $('#preloader .preloader-container').fadeOut();
   $('#preloader').delay(350).fadeOut('slow'); 
-  $('body').delay(350).css({'overflow': 'visible'});
+  $('body').delay(350).css({'overflow-y': 'visible'});
 })
