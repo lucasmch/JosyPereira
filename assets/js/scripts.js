d2d05a2fc41760afefb439ce151671b77be074fb
lucasmch <lucasgms2002@gmail.com>
2023-01-10 13:03:51 -0300

    Manutenção

--- assets/js/scripts.js
+++
@@ -1,7 +0,0 @@
-$(window).on('load', function () {
-  $('#preloader .preloader-container').fadeOut();
-  $('#preloader').delay(350).fadeOut('slow'); 
-  setTimeout(() => {
-    $('body').delay(350).css({'overflow-y': 'visible'});
-  }, 1000);
-})
